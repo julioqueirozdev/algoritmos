0897b3c5fadf8b29eb65b960f9ad2a1b243ac849
Júlio Queiroz <jcqaraujo@gmail.com>
2024-06-26 09:53:27 -0400

    Primeiro Algoritmo

---
+++ teste/sum.py
@@ -0,0 +1,12 @@
+#função de soma de dois digitos
+def sum(num1, num2):
+    return num1 + num2
+
+#atribui valores nas variáveis
+print("\tSoma de dois numeros\n")
+num1 = int(input("Digite o número: "))
+num2 = int(input("Digite o número: "))
+
+#imprime o resultado na tela
+print("")
+print("o resultado da soma é %d" %sum(num1, num2))
